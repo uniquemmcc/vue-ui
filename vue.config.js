e989c79eb4dc9fdac0414e981ed64998ced49696
Uniquemmcc <281573568@qq.com>
2020-04-29 00:56:17 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const resolve = dir => path.join(__dirname, dir)
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/' : '/'
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/abc' : '/'
 
 module.exports = {
   lintOnSave: false,
